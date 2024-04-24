beb5eaf42a228b0dfda9af51396abf959c809b62
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2024-04-23 21:23:32 -0300

    feat: override jpa methods

--- src/main/java/com/guiais/todo/infra/repositories/JPATodoRepository.java
+++ src/main/java/com/guiais/todo/infra/repositories/JPATodoRepository.java
@@ -2,8 +2,15 @@ package com.guiais.todo.infra.repositories;
 
 import com.guiais.todo.domain.entities.Todo;
 import com.guiais.todo.domain.protocols.repositories.TodoRepository;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JPATodoRepository
-  extends TodoRepository, JpaRepository<Todo, UUID> {}
+  extends TodoRepository, JpaRepository<Todo, UUID> {
+  Todo save(Todo todo);
+  Optional<Todo> findById(UUID id);
+  List<Todo> findAll();
+  void deleteById(UUID id);
+}
